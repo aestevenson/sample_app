33e6e7ea82905febb2d0f613b1a494a6ecbd6bd1
Aaron Stevenson <aaron@cantina.co>
2012-06-27 07:29:13 -0400

    switched to postgres

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,19 @@ describe "StaticPages" do
 
   describe "Home Page" do
 
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
   	it "should have content 'Sample App'" do
   		visit '/static_pages/home'
   		page.should have_content('Sample App')
   	end
 
-   it "should have the title 'Home'" do
+    it "should not have a custom page title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "#{base_title} | Home")
+      page.should_not have_selector('title', :text => '| Home')
     end
 
   end
